fcf9a74a75ee18f86e68c7dc694421f5c4c95463
IvanMTD <morgantdarck@gmail.com>
2023-11-08 21:55:36 +0300

    Initial commit

---
+++ src/test/java/ru/main/passcode/PasscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.main.passcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
