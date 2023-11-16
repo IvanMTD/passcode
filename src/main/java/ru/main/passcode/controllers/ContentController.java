2a5753e49926b76ae5c5cb9e3104b11116da4576
Ivan Morgan <morgantdarck@gmail.com>
2023-11-16 17:55:42 +0300

    update 15.11.2023 - added dynamic file integration

--- src/main/java/ru/main/passcode/controllers/ContentController.java
+++ src/main/java/ru/main/passcode/controllers/ContentController.java
@@ -4,14 +4,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +27,19 @@ import ru.main.passcode.dto.ContentDTO;
 import ru.main.passcode.dto.OutgoingMessage;
 import ru.main.passcode.interfaces.RabbitMessageService;
 import ru.main.passcode.models.Content;
+import ru.main.passcode.models.Person;
 import ru.main.passcode.services.ContentService;
+import ru.main.passcode.services.PersonService;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
@@ -54,8 +67,8 @@ public class ContentController {
         rabbitSandMessage(contentService.save(file));
         prepareTotalPages();
         prepareModel(page,model);
-        //ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        //this.template.convertAndSend("/send/content",ow.writeValueAsString(new Message(Message.ADDED, contentService.findByIdContentDTO(content.getId()))));
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        this.template.convertAndSend("/send/server", ow.writeValueAsString(0));
         return "redirect:/admin/files/" + page;
     }
 
@@ -123,7 +136,7 @@ public class ContentController {
         if(afterList.size() == beforeList.size()){
             System.out.println("Размеры равны значит после удаления осталось 5 файлов");
             ContentDTO onAdded = beforeList.get(4);
-            return ow.writeValueAsString(new ResponseMessage(totalPages,requestMessage.getPage(),requestMessage.getPage(),onAdded,onDelete));
+            return ow.writeValueAsString(new ResponseMessage(totalPages,requestMessage.getPage(),requestMessage.getPage(),onAdded,onDelete,ResponseMessage.DELETED));
         }else{
             if(afterList.size() == 0){
                 System.out.println("На странице нету больше файлов");
@@ -131,12 +144,20 @@ public class ContentController {
                 if(page>0){
                     page = page - 1;
                 }
-                return ow.writeValueAsString(new ResponseMessage(totalPages,page,requestMessage.getPage(),null,onDelete));
+                return ow.writeValueAsString(new ResponseMessage(totalPages,page,requestMessage.getPage(),null,onDelete,ResponseMessage.DELETED));
             }else {
                 System.out.println("На странице еще есть файлы");
-                return ow.writeValueAsString(new ResponseMessage(totalPages,requestMessage.getPage(),requestMessage.getPage(),null,onDelete));
+                return ow.writeValueAsString(new ResponseMessage(totalPages,requestMessage.getPage(),requestMessage.getPage(),null,onDelete,ResponseMessage.DELETED));
             }
         }
+    }
+
+    @MessageMapping("/answer")
+    //@SendTo("/send/server")
+    public void clientAnswer(@NonNull String page) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        int currentPage = Integer.parseInt(page.substring(1,page.length()-1)) - 1;
+        this.template.convertAndSend("/send/server", ow.writeValueAsString(new DataMessage(currentPage, totalPages, contentService.findAllByPageable(PageRequest.of(currentPage, itemOnPage)))));
     }
 
     /*@Scheduled(fixedDelay = 1000)
@@ -179,19 +200,24 @@ public class ContentController {
 
     @Data
     static class ResponseMessage {
+        private static final int ADDED = 1;
+        private static final int DELETED = 2;
+
+        private int status;
         private int totalPage;
         private int currentPage;
         private int oldPage;
         private ContentDTO onAdd;
         private ContentDTO onDelete;
 
-        public ResponseMessage(int totalPage, int currentPage, int oldPage, ContentDTO onAdd, ContentDTO onDelete) {
+        public ResponseMessage(int totalPage, int currentPage, int oldPage, ContentDTO onAdd, ContentDTO onDelete, int status) {
             System.out.println("current: " + currentPage + " old: " + oldPage);
             this.totalPage = totalPage;
             this.currentPage = currentPage;
             this.oldPage = oldPage;
             this.onAdd = onAdd;
             this.onDelete = onDelete;
+            this.status = status;
         }
     }
 
@@ -202,6 +228,20 @@ public class ContentController {
         public RequestMessage(int id, int page) {
             this.id = id;
             this.page = page;
+        }
+    }
+
+    @Data
+    static class DataMessage{
+        private int currentPage;
+        private int totalPages;
+        private List<ContentDTO> content;
+
+        public DataMessage(int currentPage, int totalPages, List<ContentDTO> content) {
+            System.out.println("Размер листа: " + content.size() + " текущая страница " + currentPage);
+            this.currentPage = currentPage;
+            this.totalPages = totalPages;
+            this.content = content;
         }
     }
 }
--- src/main/resources/static/js/content-control.js
+++ src/main/resources/static/js/content-control.js
@@ -5,6 +5,9 @@ stompClient.onConnect = (frame) => {
     stompClient.subscribe('/send/content', (content) => {
         setupContent(content.body);
     });
+    stompClient.subscribe('/send/server', (message) => {
+        readMessage(message.body);
+    });
 };
 
 stompClient.onWebSocketError = (error) => {
@@ -15,6 +18,60 @@ stompClient.onStompError = (frame) => {
     console.error('Broker reported error: ' + frame.headers['message']);
     console.error('Additional details: ' + frame.body);
 };
+
+function readMessage(message){
+    let page = document.getElementById('pageNum').textContent;
+    if(message.length > 1){
+        console.log(message)
+        const serverInformation = JSON.parse(message);
+        let currentPage = serverInformation.currentPage;
+        let totalPages = serverInformation.totalPages;
+        let content = serverInformation.content;
+
+        if((page - 1) === currentPage) {
+            $('#array-structure').html('');
+            content.forEach((onAdded) => {
+                console.log(currentPage)
+                $('#array-structure').append(
+                    '                     <div class="media-object stack-for-small" id="element_' + onAdded.id + '" style="border: solid 1px lightgray; padding: 10px; background-color: whitesmoke">\n' +
+                    '                        <div class="media-object-section medium-6">\n' +
+                    '                            <video class="thumbnail" width="300" height="200"  muted controls="controls">\n' +
+                    '                                <source src="' + onAdded.fullPath + '" type="video/mp4" />\n' +
+                    '                            </video>\n' +
+                    '                        </div>\n' +
+                    '                        <div class="media-object-section" style="position: relative">\n' +
+                    '                            <div class="medium-12">\n' +
+                    '                                <ul class="spisok">\n' +
+                    '                                    <li style="font-size: 14px">Имя: <span>' + onAdded.fileName + '</span></li>\n' +
+                    '                                    <li style="font-size: 14px">Дата: <span>' + onAdded.placedAt + '</span></li> <!-- исправить на дату записи видео в базе -->\n' +
+                    '                                    <li style="font-size: 14px">Размер файла: <span>' + onAdded.fileSize + '</span></li> <!-- Размер сделать в гигабайтах -->\n' +
+                    '                                </ul>\n' +
+                    '                            </div>\n' +
+                    '                            <div class="small-12 row button-group" style="position: absolute; bottom: 10px">\n' +
+                    '                                <div class="small-6 column">\n' +
+                    '                                    <form method="POST" action="/admin/files/get/photo/' + onAdded.id + '">\n' +
+                    '                                        <input type="submit" value="Фото" class="submit small button disabled">\n' +
+                    '                                    </form>\n' +
+                    '                                </div>\n' +
+                    '                                <div class="small-6 column">\n' +
+                    '                                    <form method="POST" action="/admin/files/delete/' + onAdded.id + '">\n' +
+                    '                                        <input class="alert small button" type="submit" value="Удалить">\n' +
+                    '                                    </form>\n' +
+                    '                                </div>\n' +
+                    '                            </div>\n' +
+                    '                        </div>\n' +
+                    '                    </div>'
+                )
+            })
+        }
+    }else{
+        let page = document.getElementById('pageNum').textContent;
+        stompClient.publish({
+            destination: "/app/answer",
+            body: JSON.stringify(page)
+        })
+    }
+}
 
 function setupContent(content){
     let json = content;
@@ -119,10 +176,6 @@ function deleteElement(id,page){
         destination: "/app/delete-element",
         body: JSON.stringify({'id': id,'page':page})
     })
-}
-
-function addElement(){
-
 }
 
 /*setInterval(update,1000)
